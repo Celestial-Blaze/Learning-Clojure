7d1467813acab2e7bc4b26f867c905e3af856459
Celestial Blaze <152753428+Celestial-Blaze@users.noreply.github.com>
2025-04-27 19:12:16 -0400

    added Megdalia's binary search code

--- src/learning_clojure/algorithms.clj
+++ src/learning_clojure/algorithms.clj
@@ -1,5 +1,41 @@
 (ns learning-clojure.algorithms)
 
+;; Binary search function using recursion
+(defn binary-search [vector, value] ; Use Kebab-case for function names with Clojure
+
+  (if (empty? vector) ; Checking if the vector is empty -- if it is, return false
+    false ; Then expression - return false, the value is not in the list
+
+    (let [vector-length (count vector), pivot-index (quot vector-length 2), pivot (nth vector pivot-index)] ; Finding the middle element
+      ;; (println "\n-------------------------")
+      ;; (println "Current Vector: " vector)
+      ;; (println "Pivot-index: " pivot-index)
+      ;; (println "Pivot in Vector: " pivot)
+
+      ; Using a cond bc there's multiple options
+      (cond
+        ;; If the pivot is the value we're looking for, return true
+        (= value pivot) true
+
+        ; If the pivot is less than the value, search the left side of the list
+        (< value pivot)
+        (do ; Add a do so Clojure knows it should run the following lines
+          ;; (println "Searching left...")
+          (binary-search (subvec vector 0 pivot-index) value)
+          )
+        (> value pivot) ; Don't keep the (let...) on this line...Clojure freaks out
+        (let [new-right-index (+ pivot-index 1)
+              right-vector (subvec vector new-right-index vector-length)]
+
+          ;; (println "Searching right...")
+          ;; (println "New right vector: " right-vector)
+          (binary-search right-vector value)
+          )
+        ;; :else false ; Don't really need
+        ))))
+
+
+;; Merge Sort
 (defn merge-sorted [left right]                             ; this one merges sorted sub "collections"
   (cond                                                     ; this is supposed to be a pattern matching technique
     (empty? left) right
--- src/learning_clojure/core.clj
+++ src/learning_clojure/core.clj
@@ -1,13 +1,24 @@
 (ns learning-clojure.core
   (:require [learning-clojure.algorithms :as algo]))
 
-;(defn greet []
-;      (println "Hello, World!"))
 
 (defn -main []
-    (println "Sorted result:" (algo/merge-sort [8 3 5 2 9 1]))
-    (println "Sorted result:" (algo/merge-sort []))
-    (println "Sorted result:" (algo/merge-sort [1]))
-    (println "Sorted result:" (algo/merge-sort [1 2 3 4 5 6]))
-    (println "Sorted result:" (algo/merge-sort [100 294 1 12 -5 9 37 42 0]))
+  ;; Calling binary search
+  (println"----------")
+  (println "\nLooking for 3 in [1, 2, 3, 4, 5]..." (algo/binary-search [1, 2, 3, 4, 5] 3))
+  (println "\nLooking for 2 in [1, 2, 3, 4]..." (algo/binary-search [1, 2, 3, 4] 2))
+  (println "\nLooking for 4 in [1, 2, 3, 4]..." (algo/binary-search [1, 2, 3, 4] 4))
+  (println "\nLooking for -24 in [17, 24, 31]..." (algo/binary-search [17, 24, 31] -24))
+  (println "\nLooking for 15 in [11, 22, 33, 44]..." (algo/binary-search [11, 22, 33, 44] 15))
+  (println "\nLooking for 42 in [42]..." (algo/binary-search [42] 42))
+  (println "\nLooking for 99 in []..." (algo/binary-search [] 99))
+  (println "\nLooking for 2 in [1, 2, 2, 3, 4]..." (algo/binary-search [1, 2, 2, 3, 4] 2))
+  (println "\nLooking for 10099 in [12, 25, 68, 89, 109, 111, 1029, 10098]..." (algo/binary-search [12, 25, 68, 89, 109, 111, 1029, 10098] 10099))
+  (println "\nLooking for -15 in [-24, -20, -19, -18, -17, -16, 22, 33, 44]..." (algo/binary-search [-24, -20, -19, -18, -17, -16, 22, 33, 44] -15))
+  ;; Calling Merge-Sort
+  (println "Sorted result:" (algo/merge-sort [8 3 5 2 9 1]))
+  (println "Sorted result:" (algo/merge-sort []))
+  (println "Sorted result:" (algo/merge-sort [1]))
+  (println "Sorted result:" (algo/merge-sort [1 2 3 4 5 6]))
+  (println "Sorted result:" (algo/merge-sort [100 294 1 12 -5 9 37 42 0]))
   )
